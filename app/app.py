94cecbe1849b288781c4c3964a754ad837c9c225
daichi <daichi970918@gmail.com>
2021-01-08 08:38:20 +0900

    ログイン機能完成

--- app/app.py
+++ app/app.py
@@ -1,22 +1,22 @@
-from flask import Flask,render_template,request
-from models.models import YarukotoList
+from flask import Flask,render_template,request,session,redirect,url_for
+from models.models import YarukotoList,User
 from models.database import db_session
+from app import key
+from hashlib import sha256
+
 app = Flask(__name__)
+app.secret_key = key.SECRET_KEY
 
 
 @app.route("/")
 @app.route("/index")
 def index():
-    name = request.args.get("name")
+  if "user_name" in session:
+    name = session["user_name"]
     all_yarukoto = YarukotoList.query.all()
     return render_template("index.html",name=name,all_yarukoto=all_yarukoto)
-
-
-@app.route("/index",methods=["post"])
-def post():
-    name = request.form["name"]
-    all_yarukoto = YarukotoList.query.all()
-    return render_template("index.html",name=name,all_yarukoto=all_yarukoto)
+  else:
+    return redirect(url_for("top",status="logout"))
 
 @app.route("/add",methods=["post"])
 def add():
@@ -25,14 +25,15 @@ def add():
     content = YarukotoList(title,body)
     db_session.add(content)
     db_session.commit()
-    return index()
+    return redirect(url_for("index"))
+
 @app.route("/update",methods=["post"])
 def update():
     content = YarukotoList.query.filter_by(id=request.form["update"]).first()
     content.title = request.form["title"]
     content.body = request.form["body"]
     db_session.commit()
-    return index()
+    return redirect(url_for("index"))
 
 @app.route("/delete",methods=["post"])
 def delete():
@@ -41,7 +42,56 @@ def delete():
         content =YarukotoList.query.filter_by(id=id).first()
         db_session.delete(content)
     db_session.commit()
-    return index()
+    return redirect(url_for("index"))
+
+@app.route("/top")
+def top():
+    status = request.args.get("status")
+    return render_template("top.html",status=status)
+
+
+@app.route("/login",methods=["post"])
+def login():
+    user_name = request.form["user_name"]
+    user = User.query.filter_by(user_name=user_name).first()
+    if user:
+        password = request.form["password"]
+        hashed_password = sha256((user_name + password + key.SALT).encode("utf-8")).hexdigest()
+        if user.hashed_password == hashed_password:
+            session["user_name"] = user_name
+            return redirect(url_for("index"))
+        else:
+            return redirect(url_for("top",status="wrong_password"))
+    else:
+        return redirect(url_for("top",status="user_notfound"))
+
+
+@app.route("/newcomer")
+def newcomer():
+    status = request.args.get("status")
+    return render_template("newcomer.html",status=status)
+
+
+@app.route("/registar",methods=["post"])
+def registar():
+    user_name = request.form["user_name"]
+    user = User.query.filter_by(user_name=user_name).first()
+    if user:
+        return redirect(url_for("newcomer",status="exist_user"))
+    else:
+        password = request.form["password"]
+        hashed_password = sha256((user_name + password + key.SALT).encode("utf-8")).hexdigest()
+        user = User(user_name, hashed_password)
+        db_session.add(user)
+        db_session.commit()
+        session["user_name"] = user_name
+        return redirect(url_for("index"))
+
+
+@app.route("/logout")
+def logout():
+    session.pop("user_name", None)
+    return redirect(url_for("top",status="logout"))
 
 if __name__ == "__main__":
     app.run(debug=True)
